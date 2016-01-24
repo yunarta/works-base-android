83e6d78f5e60ac2cf57bfe65d3dc2229e66be920
Yunarta Kartawahyudi <yunarta.kartawahyudi@gmail.com>
2016-01-25 01:10:16 +0800

    Added wait for multiple object

---
+++ library/src/main/java/com/mobilesolutionworks/android/bolts/BoltsWaitForMultipleObjects.java
@@ -0,0 +1,42 @@
+package com.mobilesolutionworks.android.bolts;
+
+import android.util.SparseArray;
+import bolts.Task;
+import bolts.TaskCompletionSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yunarta on 24/1/16.
+ */
+public class BoltsWaitForMultipleObjects {
+
+    private final int mCount;
+
+    private SparseArray<TaskCompletionSource<Void>> mTasks;
+
+    public BoltsWaitForMultipleObjects(int... objects) {
+        mCount = objects.length;
+
+        mTasks = new SparseArray<>();
+        for (int object : objects) {
+            mTasks.append(object, new TaskCompletionSource<Void>());
+        }
+    }
+
+    public Task completion() {
+        int size = mTasks.size();
+
+        List<Task<?>> tasks = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            tasks.add(mTasks.valueAt(i).getTask());
+        }
+
+        return Task.whenAll(tasks);
+    }
+
+    public void completed(int key) {
+        mTasks.get(key).trySetResult(null);
+    }
+}
