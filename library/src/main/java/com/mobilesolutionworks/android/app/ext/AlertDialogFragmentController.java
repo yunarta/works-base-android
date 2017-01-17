4d5d23fd00795a882a2be50c60f013e4da42525f
Yunarta Kartawahyudi <yunarta@vcube.co.jp>
2017-01-17 12:11:53 +0800

    Add todo

--- library/src/main/java/com/mobilesolutionworks/android/app/ext/AlertDialogFragmentController.java
+++ library/src/main/java/com/mobilesolutionworks/android/app/ext/AlertDialogFragmentController.java
@@ -2,11 +2,8 @@ package com.mobilesolutionworks.android.app.ext;
 
 import com.mobilesolutionworks.android.app.WorksDialogFragment;
 import com.mobilesolutionworks.android.bolts.BoltsWorksController3;
-import com.pacoworks.rxtuples.RxTuples;
 
-import rx.Observable;
 import rx.functions.Action2;
-import rx.functions.Func2;
 
 /**
  * Created by yunarta on 7/12/16.
@@ -16,15 +13,16 @@ public class AlertDialogFragmentController<Data> extends BoltsWorksController3<W
 
     private final Data mData;
 
-    private final Action2<Integer, Data> action;
+    private final Action2<Integer, Data> mAction;
 
     public AlertDialogFragmentController(Data mData, Action2<Integer, Data> action) {
         this.mData = mData;
-        this.action = action;
+        this.mAction = action;
     }
 
     public void postResult(int choiceRes) {
-        action.call(choiceRes, mData);
+        // todo https://mint.splunk.com/dashboard/project/99f96009/errors/8347258028, mAction was  null when user clicked the dialog button?
+        mAction.call(choiceRes, mData);
     }
 
 }
