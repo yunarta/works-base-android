7eb38a5227abe1c9a5d1f885e75fc1e8511a002e
Lucas Nelaupe <lucas.nelaupe@vcube.co.jp>
2017-02-09 16:43:58 +0800

    Add works compat dialog fragment

---
+++ library/src/main/java/com/mobilesolutionworks/android/app/WorksCompatDialogFragment.java
@@ -0,0 +1,94 @@
+package com.mobilesolutionworks.android.app;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.View;
+
+/**
+ * Created by lucas34990 on 9/2/17.
+ */
+
+public class WorksCompatDialogFragment extends AppCompatDialogFragment implements WorksFragmentBase {
+
+    private WorksControllerManager mController;
+
+    private transient Object mTransientData;
+
+    private int mTargetControllerId;
+
+    private int mTargetControllerRequestCode;
+
+    public void setTransientData(Object transientData) {
+        mTransientData = transientData;
+    }
+
+    public void setTargetController(int id) {
+        setTargetController(id, 0);
+    }
+
+    public void setTargetController(int id, int requestCode) {
+        mTargetControllerId = id;
+        mTargetControllerRequestCode = requestCode;
+    }
+
+    public int getTargetControllerId() {
+        return mTargetControllerId;
+    }
+
+    public int getTargetControllerRequestCode() {
+        return mTargetControllerRequestCode;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WorksControllerManager.Loader loader = (WorksControllerManager.Loader) getLoaderManager().initLoader(0, null, new WorksControllerManager.LoaderCallbacks(getActivity(), mTransientData));
+        mController = loader.getController();
+
+        if (savedInstanceState != null) {
+            mTargetControllerId = savedInstanceState.getInt("targetControllerId");
+            mTargetControllerRequestCode = savedInstanceState.getInt("targetControllerRequestCode");
+        }
+    }
+
+    public WorksControllerManager getControllerManager() {
+        return mController;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mController.onRestoreInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mController.dispatchPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mController.dispatchResume();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt("targetController", mTargetControllerId);
+        outState.putInt("targetControllerRequestCode", mTargetControllerRequestCode);
+
+        mController.dispatchOnSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    public void postControllerResult(int id, int requestCode, int resultCode, Object data) {
+        WorksController controller = mController.getController(id);
+        if (controller != null) {
+            controller.onControllerResult(requestCode, resultCode, data);
+        }
+    }
+
+}
